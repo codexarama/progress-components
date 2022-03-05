0ff6dc81eaf8c1d0bca539eb5fd08995c3263409
Amandine Arias <amandine.arias@gmail.com>
2022-03-05 15:02:18 +0100

    feat (Choice) : bars button illustration

--- src/components/Choice/index.jsx
+++ src/components/Choice/index.jsx
@@ -1,12 +1,28 @@
+import { useState } from 'react';
+
 import './choice.css';
 
 export default function Choice() {
+  const [isHovered, setIsHovered] = useState(false);
+
   return (
     <main className="choice">
       <h1 className="choice_title">Progression illustrations</h1>
       <section className="choice_buttons">
-        <button className="choice_bars">
-          <h2>bars</h2>
+        <button
+          className="choice_bars"
+          onMouseEnter={() => setIsHovered(true)}
+          onMouseLeave={() => setIsHovered(false)}
+        >
+          {isHovered ? (
+            <progress max="100" value="60">
+              <div className="choice_bars--illustration">
+                <div></div>
+              </div>
+            </progress>
+          ) : (
+            <h2>bars</h2>
+          )}
         </button>
         <button className="choice_steps">
           <h2>steps</h2>
