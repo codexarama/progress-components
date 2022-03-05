b77f2e2708f7e027908f81adfcaea84f1e01c8ad
Amandine Arias <amandine.arias@gmail.com>
2022-03-05 19:26:10 +0100

    feat (Choice) : dynamic & illustrated button appearance

--- src/components/Choice/index.jsx
+++ src/components/Choice/index.jsx
@@ -1,11 +1,22 @@
 import { useState } from 'react';
-
+import circleFilled from '../../assets/CodiconCircleFilled.svg';
+import circleOutline from '../../assets/CodiconCircleOutline.svg';
 import './choice.css';
 
 export default function Choice() {
+  const [isBar, setIsBar] = useState('');
+  const [isStep, setIsStep] = useState('');
   const [isHovered, setIsHovered] = useState(false);
 
-  function toggle() {
+  function choiceBar() {
+    setIsBar(true);
+    setIsStep(false);
+    setIsHovered(!isHovered);
+  }
+
+  function choiceStep() {
+    setIsStep(true);
+    setIsBar(false);
     setIsHovered(!isHovered);
   }
 
@@ -14,19 +25,31 @@ export default function Choice() {
       <h1 className="choice_title">Progression illustrations</h1>
       <section className="choice_buttons">
         <button
-          className="choice_bars"
-          onMouseEnter={toggle}
-          onMouseLeave={toggle}
+          className="choice_bar"
+          onMouseEnter={choiceBar}
+          onMouseLeave={choiceBar}
         >
-          {isHovered ? (
-            <progress max="100" value="60">
-            </progress>
+          {isBar && isHovered ? (
+            <progress max="100" value="60"></progress>
           ) : (
             <h2>bars</h2>
           )}
         </button>
-        <button className="choice_steps">
-          <h2>steps</h2>
+        <button
+          className="choice_step"
+          onMouseEnter={choiceStep}
+          onMouseLeave={choiceStep}
+        >
+          {isStep && isHovered ? (
+            <>
+              <img src={circleFilled} alt="filled circle icon" />
+              <img src={circleFilled} alt="filled circle icon" />
+              <img src={circleFilled} alt="filled circle icon" />
+              <img src={circleOutline} alt="outline circle icon" />
+            </>
+          ) : (
+            <h2>steps</h2>
+          )}
         </button>
       </section>
     </main>
