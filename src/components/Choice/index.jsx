98dd64b9d410fad9085dfdd6d3318bdbf303b294
Amandine Arias <amandine.arias@gmail.com>
2022-03-05 15:53:40 +0100

    feat (Choice) : improve code logic for hovering handler

--- src/components/Choice/index.jsx
+++ src/components/Choice/index.jsx
@@ -5,14 +5,18 @@ import './choice.css';
 export default function Choice() {
   const [isHovered, setIsHovered] = useState(false);
 
+  function toggle() {
+    setIsHovered(!isHovered);
+  }
+
   return (
     <main className="choice">
       <h1 className="choice_title">Progression illustrations</h1>
       <section className="choice_buttons">
         <button
           className="choice_bars"
-          onMouseEnter={() => setIsHovered(true)}
-          onMouseLeave={() => setIsHovered(false)}
+          onMouseEnter={toggle}
+          onMouseLeave={toggle}
         >
           {isHovered ? (
             <progress max="100" value="60">
