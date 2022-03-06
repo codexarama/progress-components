abe5c8ea78d1eaf7ef63720e305a2b65aef4e427
Amandine Arias <amandine.arias@gmail.com>
2022-03-06 17:26:23 +0100

    feat (Choice) : add ProgressBar component

--- src/components/Choice/index.jsx
+++ src/components/Choice/index.jsx
@@ -1,11 +1,17 @@
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
+
 import circleFilled from '../../assets/CodiconCircleFilled.svg';
 import circleOutline from '../../assets/CodiconCircleOutline.svg';
+
+import ProgressBar from '../ProgressBar';
+
 import './choice.css';
 
 export default function Choice() {
   const [isBar, setIsBar] = useState('');
+  const [isBarOpen, setIsBarOpen] = useState(false);
   const [isStep, setIsStep] = useState('');
+
   const [isHovered, setIsHovered] = useState(false);
 
   function choiceBar() {
@@ -20,38 +26,61 @@ export default function Choice() {
     setIsHovered(!isHovered);
   }
 
+  // close modal when open
+  function toggleBar() {
+    setIsBarOpen(!isBarOpen);
+  }
+
+  // close modal by pressing escape key when keyboard navigation
+  function escToClose(e) {
+    if (e.key === 'Escape') {
+      toggleBar();
+    }
+  }
+
+  // detects when escape key pressed to close the modal
+  // and stops event propagation when occured
+  useEffect(() => {
+    window.addEventListener('keydown', escToClose);
+    return () => window.removeEventListener('keydown', escToClose);
+  });
+
   return (
-    <main className="choice">
-      <h1 className="choice_title">Progression illustrations</h1>
-      <section className="choice_buttons">
-        <button
-          className="choice_bar"
-          onMouseEnter={choiceBar}
-          onMouseLeave={choiceBar}
-        >
-          {isBar && isHovered ? (
-            <progress max="100" value="60"></progress>
-          ) : (
-            <h2>bars</h2>
-          )}
-        </button>
-        <button
-          className="choice_step"
-          onMouseEnter={choiceStep}
-          onMouseLeave={choiceStep}
-        >
-          {isStep && isHovered ? (
-            <>
-              <img src={circleFilled} alt="filled circle icon" />
-              <img src={circleFilled} alt="filled circle icon" />
-              <img src={circleFilled} alt="filled circle icon" />
-              <img src={circleOutline} alt="outline circle icon" />
-            </>
-          ) : (
-            <h2>steps</h2>
-          )}
-        </button>
-      </section>
-    </main>
+    <>
+      <main className="choice">
+        <h1 className="choice_title">Progression illustrations</h1>
+        <section className="choice_buttons">
+          <button
+            className="choice_bar"
+            onMouseEnter={choiceBar}
+            onMouseLeave={choiceBar}
+            onClick={toggleBar}
+          >
+            {isBar && isHovered ? (
+              <progress max="100" value="60"></progress>
+            ) : (
+              <h2>bars</h2>
+            )}
+          </button>
+          <button
+            className="choice_step"
+            onMouseEnter={choiceStep}
+            onMouseLeave={choiceStep}
+          >
+            {isStep && isHovered ? (
+              <>
+                <img src={circleFilled} alt="filled circle icon" />
+                <img src={circleFilled} alt="filled circle icon" />
+                <img src={circleFilled} alt="filled circle icon" />
+                <img src={circleOutline} alt="outline circle icon" />
+              </>
+            ) : (
+              <h2>steps</h2>
+            )}
+          </button>
+        </section>
+      </main>
+      <ProgressBar bar={isBarOpen} close={toggleBar} done="70" />
+    </>
   );
 }
