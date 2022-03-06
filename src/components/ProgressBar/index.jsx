84a2a8917c34d5c3eefcf98908e94cfaadc901c1
Amandine Arias <amandine.arias@gmail.com>
2022-03-06 17:24:12 +0100

    feat (ProgressBar) : create component

--- src/components/ProgressBar/index.jsx
+++ src/components/ProgressBar/index.jsx
@@ -1,7 +1,44 @@
-import React from 'react'
+import { useState } from 'react';
+import { createPortal } from 'react-dom';
+import './progressBar.css';
 
-export default function ProgressBar() {
-  return (
-    <div>ProgressBar</div>
-  )
+export default function ProgressBar({ bar, close, done }) {
+  const [style, setStyle] = useState({});
+
+  setTimeout(() => {
+    const doneStyle = {
+      opacity: 1,
+      width: `${done}%`,
+    };
+    setStyle(doneStyle);
+  }, 200);
+
+  return createPortal(
+    <>
+      {bar ? (
+        <main
+          autoFocus
+          className="modal"
+          role="main"
+          // close modal when click outside of it
+          onClick={() => {
+            close();
+          }}
+        >
+          <button className="modal_close" aria-label="Close" onClick={close}>
+            X
+          </button>
+          <h1>Custom Progress Bars</h1>
+          <section className='progress_bar' >
+            <div className="progress_bar--bg">
+              <div className="progress_bar--progress" style={style}>
+                {done}%
+              </div>
+            </div>
+          </section>
+        </main>
+      ) : null}
+    </>,
+    document.body
+  );
 }
