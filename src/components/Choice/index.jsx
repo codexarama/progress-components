aa8ea1bbb248c873be720bc04cd4f49cc2179128
Amandine Arias <amandine.arias@gmail.com>
2022-03-08 13:01:27 +0100

    feat (Choice) : clicking actions
    conditional render by clicking chosen button

--- src/components/Choice/index.jsx
+++ src/components/Choice/index.jsx
@@ -1,25 +1,41 @@
 import { useState } from 'react';
 import dots from '../../assets/dots.svg';
+import ProgressBar from '../ProgressBar';
+import ProgressStep from '../ProgressStep';
 import './choice.css';
 
 export default function Choice({ label }) {
   const [isHovered, setIsHovered] = useState(false);
+  const [isOpen, setIsOpen] = useState(false);
+
+  const handleClick = () => {
+    setIsOpen(!isOpen);
+  };
 
   return (
-    <button
-      className={label ? 'choice_bar' : 'choice_step'}
-      onMouseEnter={() => setIsHovered(true)}
-      onMouseLeave={() => setIsHovered(false)}
-    >
-      {isHovered ? (
-        label && isHovered ? (
-          <progress max="100" value="60"></progress>
+    <>
+      <button
+        className={label ? 'choice_bar' : 'choice_step'}
+        onMouseEnter={() => setIsHovered(true)}
+        onMouseLeave={() => setIsHovered(false)}
+        onClick={handleClick}
+      >
+        {isHovered ? (
+          label && isHovered ? (
+            <progress max="100" value="60"></progress>
+          ) : (
+            <img src={dots} alt="filled circle icon" />
+          )
         ) : (
-          <img src={dots} alt="filled circle icon" />
-        )
+          <h2>{label ? 'bar' : 'steps'}</h2>
+        )}
+      </button>
+
+      {label ? (
+        <ProgressBar progressBar={isOpen} close={handleClick} done="70" />
       ) : (
-        <h2>{label ? 'bar' : 'steps'}</h2>
+        <ProgressStep progressStep={isOpen} close={handleClick} />
       )}
-    </button>
+    </>
   );
 }
