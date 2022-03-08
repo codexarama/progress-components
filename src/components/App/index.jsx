e179c37e363203b54c5ddffd18c2a1c539754ae1
Amandine Arias <amandine.arias@gmail.com>
2022-03-08 11:36:12 +0100

    refactor (Choice) : improve logic with less code

--- src/components/App/index.jsx
+++ src/components/App/index.jsx
@@ -1,7 +1,16 @@
 import Choice from '../Choice';
-
 import './app.css';
 
 export default function App() {
-  return <Choice />;
+  const isBarsButton = true
+
+  return (
+    <main className="choice">
+      <h1 className="choice_title">Progression illustrations</h1>
+      <section className="choice_buttons">
+        <Choice label={isBarsButton} />
+        <Choice label={!isBarsButton} />
+      </section>
+    </main>
+  );
 }
--- src/components/Choice/index.jsx
+++ src/components/Choice/index.jsx
@@ -1,83 +1,25 @@
-import { useState, useEffect } from 'react';
-
+import { useState } from 'react';
 import dots from '../../assets/dots.svg';
-
-import ProgressBar from '../ProgressBar';
-
 import './choice.css';
 
-export default function Choice() {
-  const [isBar, setIsBar] = useState('');
-  const [isBarOpen, setIsBarOpen] = useState(false);
-  
-  const [isStep, setIsStep] = useState('');
-
+export default function Choice({ label }) {
   const [isHovered, setIsHovered] = useState(false);
 
-  function choiceBar() {
-    setIsBar(true);
-    setIsStep(false);
-    setIsHovered(!isHovered);
-  }
-
-  function choiceStep() {
-    setIsStep(true);
-    setIsBar(false);
-    setIsHovered(!isHovered);
-  }
-
-  // close modal when open
-  function toggleBar() {
-    setIsBarOpen(!isBarOpen);
-  }
-
-  // close modal by pressing escape key when keyboard navigation
-  function escToClose(e) {
-    if (e.key === 'Escape') {
-      toggleBar();
-    }
-  }
-
-  // detects when escape key pressed to close the modal
-  // and stops event propagation when occured
-  useEffect(() => {
-    window.addEventListener('keydown', escToClose);
-    return () => window.removeEventListener('keydown', escToClose);
-  });
-
   return (
-    <>
-      <main className="choice">
-        <h1 className="choice_title">Progression illustrations</h1>
-        <section className="choice_buttons">
-          <button
-            className="choice_bar"
-            onMouseEnter={choiceBar}
-            onMouseLeave={choiceBar}
-            onClick={toggleBar}
-          >
-            {isBar && isHovered ? (
-              <progress max="100" value="60"></progress>
-            ) : (
-              <h2>bars</h2>
-            )}
-          </button>
-          <button
-            className="choice_step"
-            onMouseEnter={choiceStep}
-            onMouseLeave={choiceStep}
-          >
-            {isStep && isHovered ? (
-              <>
-                <img src={dots} alt="filled circle icon" />
-              </>
-            ) : (
-              <h2>steps</h2>
-            )}
-          </button>
-        </section>
-      </main>
-      <ProgressBar bar={isBarOpen} close={toggleBar} done="70" />
-    </>
+    <button
+      className={label ? 'choice_bar' : 'choice_step'}
+      onMouseEnter={() => setIsHovered(true)}
+      onMouseLeave={() => setIsHovered(false)}
+    >
+      {isHovered ? (
+        label && isHovered ? (
+          <progress max="100" value="60"></progress>
+        ) : (
+          <img src={dots} alt="filled circle icon" />
+        )
+      ) : (
+        <h2>{label ? 'bar' : 'steps'}</h2>
+      )}
+    </button>
   );
 }
