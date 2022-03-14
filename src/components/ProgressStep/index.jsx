91c87d0a9e0ef25ddccbf26047f0b7397eb66745
Amandine Arias <amandine.arias@gmail.com>
2022-03-14 16:36:18 +0100

    feat (ProgressStep) : 3 steps and their content

--- src/components/ProgressStep/index.jsx
+++ src/components/ProgressStep/index.jsx
@@ -1,5 +1,6 @@
 import { createPortal } from 'react-dom';
 
+import { Stepper, StepLabel, Step } from '@material-ui/core';
 import Step1 from './Step1';
 import Step2 from './Step2';
 import Step3 from './Step3';
@@ -7,6 +8,17 @@ import Step3 from './Step3';
 import './progressStep.css';
 
 export default function ProgressStep({ progressStep, close }) {
+  function stepContent(step) {
+    switch (step) {
+      case 1:
+        return <Step1 />;
+      case 2:
+        return <Step2 />;
+      case 3:
+        return <Step3 />;
+        default: console.log(`progress : step ${step} / 3`);
+    }
+  }
   return createPortal(
     <>
       {progressStep ? (
@@ -16,9 +28,18 @@ export default function ProgressStep({ progressStep, close }) {
           </button>
           <h1>Custom Progress Steps</h1>
           <section className="progress_step">
-            <Step1 />
-            <Step2 />
-            <Step3 />
+            <Stepper activeStep={1} orientation="horizontal">
+              <Step>
+                <StepLabel></StepLabel>
+              </Step>
+              <Step>
+                <StepLabel></StepLabel>
+              </Step>
+              <Step>
+                <StepLabel></StepLabel>
+              </Step>
+            </Stepper>
+            {stepContent(1)}
           </section>
         </main>
       ) : null}
